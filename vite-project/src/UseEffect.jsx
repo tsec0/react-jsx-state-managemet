e8cfc6332b946dfcb0138de754794464447c40f1
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2024-08-20 16:30:12 +0300

    Update on training useEffect hook properly

--- vite-project/src/UseEffect.jsx
+++ vite-project/src/UseEffect.jsx
@@ -4,19 +4,33 @@ function App(){
 
   const [names, setNames] = useState([]);
 
+  const [selectedName, setSelectedName] = useState(null);
+
+  const [selectedNameDetails, setSelectedNamedetails] = useState(null);
+
   // teching data causes infinite loop
   // thats why we use useEffect
   // do this onece => takes a function once the Dom is rendered 
   // or when the dependancy array changes 
   useEffect(() => {
-    fetch("/names.json")
+    fetch(`/names.json`)
     .then((response) => response.json())
     .then((data) => setNames(data))
-  }, []); // emty array because we are not depending on anything
+  }, []); // this is the dependancy array
+
+  useEffect(() => {
+    if(selectedName){
+      fetch(`/${selectedName}.json`)
+        .then((response) => response.json())
+        .then((data) => setSelectedNamedetails(data))
+      }
+    }, [selectedName]); // this is the dependancy array
 
     return (
       <div>
-        Names: {names.join(", ")}
+        {/* Missing a key */}
+        {names.map((name, index) => (<button key={index} onClick={() => setSelectedName(name)}>{name}</button>))} 
+        <div>{JSON.stringify(selectedNameDetails)}</div>
       </div>
     )
   }
