7e3dc11cad196be6b777641fcac7a1ed70312813
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2024-08-21 09:56:27 +0300

    Update useRef file

--- vite-project/src/UseRef.jsx
+++ vite-project/src/UseRef.jsx
@@ -1,6 +1,19 @@
+import { useRef } from 'react';
+
+// one more way to associate state with a component
+// change the value of a reference 
+// does not cause a component rerender (convinient)
+
+// comonly useRef is for getting an html element
+// which references to that element
+
 function App(){
-    return (
+    
+  const inputRef = useRef(null);
+
+  return (
       <div>
+        <input type="text" ref={inputRef} />
       </div>
     )
   }
