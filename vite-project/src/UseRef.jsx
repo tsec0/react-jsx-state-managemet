889e955cc660060568c82a77243a45e415cc553d
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2024-08-21 10:54:41 +0300

    Update: training usage of useRef hook

--- vite-project/src/UseRef.jsx
+++ vite-project/src/UseRef.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 // one more way to associate state with a component
 // change the value of a reference 
@@ -7,13 +7,45 @@ import { useRef } from 'react';
 // comonly useRef is for getting an html element
 // which references to that element
 
+// to maintain state whidout doing any updates
+
 function App(){
-    
+
   const inputRef = useRef(null);
+
+  // auto increment id
+  const idRef = useRef(0); // but its not working fine
+
+  const [names, setNames] = useState([
+    {id: idRef.current += 1, name: "John"},
+    {id: idRef.current += 1, name: "Jane"},
+    {id: idRef.current += 1, name: "Jill"},
+  ]);
+
+  // runs only once with empty dependancy array
+  useEffect(() => {
+    inputRef.current.focus(); // the current value (set or read from)
+  }, []);
+
+  const onAddName = () => {
+    setNames([
+      ...names, 
+      {
+        id: idRef.current += 1,
+        name: inputRef.current.value,
+      },
+    ]);
+    inputRef.current.value = "";
+  };
 
   return (
       <div>
+        {names.map((item) => (
+          <div key={item.id}>{`${item.id} - ${item.name}`}</div>
+        ))}
+        {/* Uncontrolled input in React */}
         <input type="text" ref={inputRef} />
+        <button onClick={onAddName}>Add Name</button>
       </div>
     )
   }
